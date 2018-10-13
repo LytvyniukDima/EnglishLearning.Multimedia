f944f655e7c1023cfa9c4b82f7ffe3d5c22f2520
dimaLytvyniuk <dima.lytvyniuk@gmail.com>
2018-10-13 20:32:55 +0300

    Add registrations for persistence layer

--- EnglishLearning.Multimedia.Host/Startup.cs
+++ EnglishLearning.Multimedia.Host/Startup.cs
@@ -1,20 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using EnglishLearning.Multimedia.Persistence.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace EnglishLearning.Multimedia.Host
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials());
+            });
+            
+            services.PersistenceConfiguration(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,7 +39,7 @@ namespace EnglishLearning.Multimedia.Host
                 app.UseDeveloperExceptionPage();
             }
 
-            
+            app.UseCors("CorsPolicy");
         }
     }
 }
--- EnglishLearning.Multimedia.Persistence/Configuration/PersistenceSettings.cs
+++ EnglishLearning.Multimedia.Persistence/Configuration/PersistenceSettings.cs
@@ -1,5 +1,11 @@
 ﻿using EnglishLearning.Multimedia.Persistence.Abstract;
 using EnglishLearning.Multimedia.Persistence.Contexts;
+using EnglishLearning.Multimedia.Persistence.Entities.Audio;
+using EnglishLearning.Multimedia.Persistence.Entities.Text;
+using EnglishLearning.Multimedia.Persistence.Entities.Video;
+using EnglishLearning.Multimedia.Persistence.Repositories.Audio;
+using EnglishLearning.Multimedia.Persistence.Repositories.Text;
+using EnglishLearning.Multimedia.Persistence.Repositories.Video;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -12,7 +18,15 @@ namespace EnglishLearning.Multimedia.Persistence.Configuration
             services.Configure<MongoDbConfiguration>(configuration.GetSection("MongoDbConfiguration"));
 
             services.AddScoped<IMultimediaDbContext, MultimediaDbContext>();
-            //services.AddTransient<IMongoDbRepository<EnglishTask>, EnglishTaskMongoDbRepository>();
+            
+            services.AddTransient<IRepository<AudioType>, AudioTypeMongoRepository>();
+            services.AddTransient<IRepository<EnglishAudio>, EnglishAudioMongoRepository>();
+
+            services.AddTransient<IRepository<TextType>, TextTypeMongoRepository>();
+            services.AddTransient<IRepository<EnglishText>, EnglishTextMongoRepository>();
+
+            services.AddTransient<IRepository<VideoType>, VideoTypeMongoRepository>();
+            services.AddTransient<IRepository<EnglishVideo>, EnglishVideoMongoRepository>();
             
             return services;
         }
