7b487d9de46bceed6319ae6ae0deefe9fc42e692
dimaLytvyniuk <dima20-12@outlook.com>
2019-05-12 10:39:22 +0300

    Add texts backup

--- EnvironmentTools/importBackup.sh
+++ EnvironmentTools/importBackup.sh
@@ -1,1 +1,3 @@
 mongoimport --db EnglishLearning_MultimediaService --collection EnglishMultimedia_EnglishVideo --drop EnglishVideos.json
+
+mongoimport --db EnglishLearning_MultimediaService --collection EnglishMultimedia_EnglishText --drop EnglishTexts.json
--- EnvironmentTools/makeBackup.sh
+++ EnvironmentTools/makeBackup.sh
@@ -1,1 +1,3 @@
-mongoexport --db EnglishLearning_MultimediaService --collection EnglishMultimedia_EnglishVideo -o EnglishVideos.json --pretty
+#mongoexport --db EnglishLearning_MultimediaService --collection EnglishMultimedia_EnglishVideo -o EnglishVideos.json --pretty
+
+mongoexport --db EnglishLearning_MultimediaService --collection EnglishMultimedia_EnglishText -o EnglishTexts.json --pretty
