1760eee8f2eabde9c3a5d35d11a16913077da907
LytvyniukDima <dima.lytvyniuk@gmail.com>
2020-07-08 22:52:48 +0300

    Remove multimedia scripts

--- EnvironmentTools/importBackup.sh
+++
@@ -1,3 +0,0 @@
-mongoimport --db EnglishLearning_MultimediaService --collection EnglishMultimedia_EnglishVideo --drop EnglishVideos.json
-
-mongoimport --db EnglishLearning_MultimediaService --collection EnglishMultimedia_EnglishText --drop EnglishTexts.json
--- EnvironmentTools/makeBackup.sh
+++
@@ -1,3 +0,0 @@
-#mongoexport --db EnglishLearning_MultimediaService --collection EnglishMultimedia_EnglishVideo -o EnglishVideos.json --pretty
-
-mongoexport --db EnglishLearning_MultimediaService --collection EnglishMultimedia_EnglishText -o EnglishTexts.json --pretty
